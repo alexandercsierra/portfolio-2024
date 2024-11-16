9bbb92039238ab36c971f18bc8c29175dc02cfa4
Alex Sierra <alexandercsierra@gmail.com>
2024-11-16 11:28:53 -0500

    add redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,29 @@
+import { Provider } from "react-redux";
+import store from "./redux/store.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Cart from "./components/Cart.tsx";
-import Nav from "./Nav.tsx";
 import Projects from "./Projects.tsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Box } from "@mui/material";
+import RootLayout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/projects", element: <Projects /> },
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/projects", element: <Projects /> },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Box sx={{ minHeight: "100vh" }}>
-      <Nav />
+    <Provider store={store}>
       <RouterProvider router={router} />
-    </Box>
+    </Provider>
   </StrictMode>
 );
---
+++ src/redux/cartSlice.tsx
@@ -0,0 +1,59 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartFromLocalStorage = localStorage.getItem("cart");
+const initialCart = cartFromLocalStorage
+  ? JSON.parse(cartFromLocalStorage)
+  : [];
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    cart: initialCart,
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const { payload } = action;
+      const newCart = [...state.cart];
+
+      // find the index of the existing product
+      const existingProductIndex = state.cart.findIndex(
+        (product) => product.id === payload.id
+      );
+
+      console.log({ existingProductIndex, payload });
+
+      // if the product exists, increase the quantity
+      if (existingProductIndex >= 0) {
+        newCart[existingProductIndex].quantity += 1;
+      } else {
+        // if the product is new, add it to the cart
+        newCart.push({ ...payload, quantity: 1 });
+      }
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      state.cart = newCart;
+    },
+    removeFromCart: (state, action) => {
+      console.log("CART SLICE removeFromCart");
+      const { payload } = action;
+      console.log({ payload });
+      const newCart = [...state.cart];
+      const existingProductIndex = state.cart.findIndex(
+        (product) => product.id === payload.id
+      );
+      const existingProduct = state.cart[existingProductIndex];
+      if (existingProduct.quantity === 1) {
+        newCart.splice(existingProductIndex, 1);
+      } else {
+        newCart[existingProductIndex].quantity -= 1;
+      }
+      console.log({ existingProductIndex, existingProduct, newCart });
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      state.cart = newCart;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToCart, removeFromCart } = counterSlice.actions;
+
+export default counterSlice.reducer;
