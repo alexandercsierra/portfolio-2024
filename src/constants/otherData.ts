ac61d19a035950b6749827f35472849dbb2af8f9
Alex Sierra <alexandercsierra@gmail.com>
2024-11-30 20:24:30 -0500

    update resume link

--- src/constants/otherData.ts
+++ src/constants/otherData.ts
@@ -1,4 +1,6 @@
-export const resumeLink =
-  "https://drive.google.com/file/d/1pqBPMVStXEr-X65xvLbY2Gc3LcfO0_2k/view?usp=sharing";
+// export const resumeLink =
+//   "https://drive.google.com/file/d/1pqBPMVStXEr-X65xvLbY2Gc3LcfO0_2k/view?usp=sharing";
 
+export const resumeLink =
+  "https://drive.google.com/file/d/1wPlkW8DJDWZb-V4WBx3H1vJWx2-HeU9Q/view?usp=sharing";
 export const footerHeight = 40;
