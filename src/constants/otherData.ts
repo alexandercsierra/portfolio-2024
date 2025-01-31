60326942ff740736e23c7044e29f838bc7c6527c
Alexander Sierra <alexandercsierra@Alexanders-MBP.lan>
2025-01-31 14:39:42 -0500

    update resume link

--- src/constants/otherData.ts
+++ src/constants/otherData.ts
@@ -2,5 +2,5 @@
 //   "https://drive.google.com/file/d/1pqBPMVStXEr-X65xvLbY2Gc3LcfO0_2k/view?usp=sharing";
 
 export const resumeLink =
-  "https://drive.google.com/file/d/1oUwau3vHjJyHYJg8A8FCnWbX9ljk5Vrd/view?usp=sharing";
+  "https://drive.google.com/file/d/1j1gRzTtc817L6JaI_KUaerJ4ZW499tIj/view?usp=sharing";
 export const footerHeight = 40;
