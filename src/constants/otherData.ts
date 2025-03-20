81b0c50753efd09772d16f4535917b93a9335d15
alexandercsierra <alexandercsierra@gmail.com>
2025-03-20 18:38:34 -0400

    update resume link

--- src/constants/otherData.ts
+++ src/constants/otherData.ts
@@ -1,6 +1,3 @@
-// export const resumeLink =
-//   "https://drive.google.com/file/d/1pqBPMVStXEr-X65xvLbY2Gc3LcfO0_2k/view?usp=sharing";
-
 export const resumeLink =
-  "https://drive.google.com/file/d/1M8JB1eeEiDt2TIh65kP3b0tt4wKwHZ86/view?usp=sharing";
+  "https://drive.google.com/file/d/1W7-iQP9PSEBfsIXm8L2UPSd5IdT5ztmH/view?usp=sharing";
 export const footerHeight = 40;
